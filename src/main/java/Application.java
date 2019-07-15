c5983b14921007bb002ca6655a316a394e3f6fc4
CodingBeagle <kasper.rie@gmail.com>
2019-07-15 22:44:39 +0200

    Initial project commit! :D

---
+++ src/main/java/Application.java
@@ -0,0 +1,45 @@
+import org.lwjgl.*;
+import org.lwjgl.glfw.GLFWErrorCallback;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+public class Application {
+
+    // The window handle
+    private long window;
+
+    public void run() {
+        System.out.println("Hello LWJGL " + Version.getVersion() + "!");
+
+        init();
+        loop();
+    }
+
+    private void init() {
+        // Setup an error callback. The default implementation
+        // Will print the error message in System.err.
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        // Initialize GLFW. Most GLFW functions will not work before doing this.
+        if (!glfwInit() )
+            throw new IllegalStateException("Unable to initialize GLFW");
+
+        // Configure GLFW
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        // Create the window
+        window = glfwCreateWindow(800, 600, "The Goal", NULL, NULL);
+
+        if (window== NULL)
+            throw new RuntimeException("Failed to create the application window.");
+
+        
+    }
+
+    private void loop() {
+
+    }
+}
